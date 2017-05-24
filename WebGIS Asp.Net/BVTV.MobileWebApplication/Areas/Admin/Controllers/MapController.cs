8a9269f23f95713e0f3cb6f1b295e61d43b61782
fuhid <fuhi.dev@gmail.com>
2017-05-24 14:51:26 +0700

    Thêm chức năng phân quyền hiển thị bản đồ

--- WebGIS Asp.Net/BVTV.MobileWebApplication/Areas/Admin/Controllers/MapController.cs
+++ WebGIS Asp.Net/BVTV.MobileWebApplication/Areas/Admin/Controllers/MapController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BVTV.MobileWebApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,20 @@ namespace BVTV.MobileWebApplication.Areas.Admin.Controllers
     [Authorize]
     public class MapController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         // GET: Admin/Map
         public ActionResult Index()
         {
             return View();
         }
+        [Authorize]
+        public ActionResult GetRoles()
+        {
+            
+            var query = from role in db.Users.First(f => f.UserName.Equals(User.Identity.Name)).Roles
+                        select role.RoleId;
+            return Json(query.ToList(), JsonRequestBehavior.AllowGet);
+        }
     }
+
 }
