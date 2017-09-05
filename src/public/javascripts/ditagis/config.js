d01762f2bc8c04bc7ffe45af5bd0319518b8bff0
PhuongHieu <fuhi.dev@gmail.com>
2017-09-05 16:42:15 +0700

    thay config sang bvtv

--- src/public/javascripts/ditagis/config.js
+++ src/public/javascripts/ditagis/config.js
@@ -3,15 +3,33 @@ define([
 ], function () {
     'use strict';
     return {
-        baseMaps: [{
+        basemap: {
             title: 'Dữ liệu nền',
-            name: 'dulieunen',
-            url: 'https://ditagis.com:6443/arcgis/rest/services/BinhDuong/BaoVeThucVat_DLN/MapServer',
-            visible: true
-        }],
+            id: 'dulieunen',
+            url: 'https://ditagis.com:6443/arcgis/rest/services/BinhDuong/DuLieuNen/MapServer',
+            sublayers:[
+                {
+                    id: 4,
+                    title:'Hành chính huyện'
+                },
+                {
+                    id: 3,
+                    title:'Hành chính xã'
+                }, {
+                    id: 2,
+                    title:'Mặt giao thông'
+                }, {
+                    id: 1,
+                    title:'Sông hồ'
+                }, {
+                    id: 0,
+                    title:'Tim đường'
+                }
+            ]
+        },
         layers: [{
             title: 'Trồng trọt',
-            name: "trongtrot",
+            id: "trongtrot",
             url: "https://ditagis.com:6443/arcgis/rest/services/BinhDuong/BaoVeThucVat/FeatureServer/2",
             outFields: ['*'],
             permissions: [{
@@ -28,7 +46,7 @@ define([
             ]
         }, {
             title: 'Doanh nghiệp',
-            name: "doanhnghiep",
+            id: "doanhnghiep",
             url: "https://ditagis.com:6443/arcgis/rest/services/BinhDuong/BaoVeThucVat/FeatureServer/0",
             outFields: ['*'],
             permissions: [{
@@ -46,7 +64,7 @@ define([
         },
         {
             title: 'Sâu bệnh',
-            name: "saubenh",
+            id: "saubenh",
             url: "https://ditagis.com:6443/arcgis/rest/services/BinhDuong/BaoVeThucVat/FeatureServer/1",
             outFields: ['*'],
             permissions: [{
@@ -62,25 +80,25 @@ define([
             }
             ]
         },
-        // {
-        //     title: 'Quy hoạch sử dụng đất',
-        //     name: "QuyHoachSuDungDat",
-        //     url: "https://ditagis.com:6443/arcgis/rest/services/BinhDuong/BaoVeThucVat/FeatureServer/3",
-        //     outFields: ['*'],
-        //     permissions: [{
-        //         role: 1,//sở thông tin truyền thông
-        //         edit: true,
-        //         create: true,
-        //         delete: true
-        //     }, {
-        //         role: 2,//doanh nghiệp
-        //         edit: true,
-        //         create: true,
-        //         delete: true
-        //     }]
-        // }
+            // {
+            //     title: 'Quy hoạch sử dụng đất',
+            //     id: "QuyHoachSuDungDat",
+            //     url: "https://ditagis.com:6443/arcgis/rest/services/BinhDuong/BaoVeThucVat/FeatureServer/3",
+            //     outFields: ['*'],
+            //     permissions: [{
+            //         role: 1,//sở thông tin truyền thông
+            //         edit: true,
+            //         create: true,
+            //         delete: true
+            //     }, {
+            //         role: 2,//doanh nghiệp
+            //         edit: true,
+            //         create: true,
+            //         delete: true
+            //     }]
+            // }
         ],
-        zoom: 16, // Sets the zoom level based on level of detail (LOD)
+        zoom: 11, // Sets the zoom level based on level of detail (LOD)
         // center: [106.5872641, 11.0254935] // Sets the center point of view in lon/lat
         center: [106.6843694, 11.0576851]
     }
